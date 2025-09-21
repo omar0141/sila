342e66dac1d2f524c20f338c8d7879809254d5f4
omar0141 <omarran2000@hotmail.com>
2025-09-22 00:14:45 +0300

    new

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "968317019"
+    serviceWorkerVersion: "1188756849"
   }
 });
